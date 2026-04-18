dbf3e89c6baa46d13b4ed7b48ef4ae33a093c0ef
Space <antariksh.mankar43@gmail.com>
2026-04-18 13:59:15 +0530

    RequestMapping added to ProductController and createProduct method syntax fixed

--- src/main/java/com/app/ecom/controller/ProductController.java
+++ src/main/java/com/app/ecom/controller/ProductController.java
@@ -8,18 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/products")
 public class ProductController {
 
     private final ProductService productService;
-    private Object object;
-
 
     @PostMapping
-    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest)
-        return new ResponseEntity<ProductResponse>(productService.createProduct(productRequest)
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest) {
+        return new ResponseEntity<>(productService.createProduct(productRequest),
                                                     HttpStatus.CREATED);
+    }
 }
